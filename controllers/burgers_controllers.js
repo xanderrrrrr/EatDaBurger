0fa2595c6f18ef4ce34f07ec0527723d618c6bc5
Xander C <xander.canedo@gmail.com>
2019-11-19 21:04:57 -0700

    have basic functionality for getting all the burgers

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,10 +1,21 @@
-var burger = require("../models/burger.js");
-
 var express = require("express");
 
 var router = express.Router();
 
+// import the model to use it's db functions
+var burger = require("../models/burger.js");
+
 // create all our routes
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var obj = {
+            burgers: data
+        };
+        console.log(obj);
+        console.log('something')
+        res.render("index", obj)
+    });
+})
 
 // export routes for server.js to use
 module.exports = router;
