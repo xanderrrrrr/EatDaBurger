ccc297c6e84d20f526a469fe2fe66c3a93235d28
Xander C <xander.canedo@gmail.com>
2019-11-19 20:39:57 -0700

    created burgers_controllers and created basic handlebars files so all burgers display regardless of devoured logic

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,10 @@
+var burger = require("../models/burger.js");
+
+var express = require("express");
+
+var router = express.Router();
+
+// create all our routes
+
+// export routes for server.js to use
+module.exports = router;
