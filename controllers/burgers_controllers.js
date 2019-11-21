b4bf4c769cf480cf5b7af7ee104804bd5f6d0c5e
Xander C <xander.canedo@gmail.com>
2019-11-20 17:01:41 -0700

    have working for creating and deleting via ui and api

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -8,14 +8,40 @@ var burger = require("../models/burger.js");
 // create all our routes
 router.get("/", function(req, res) {
     burger.all(function(data) {
-        var obj = {
+        var burgerObj = {
             burgers: data
         };
-        console.log(obj);
+        console.log(burgerObj);
         console.log('something')
-        res.render("index", obj)
+        res.render("index", burgerObj)
     });
 })
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+        } else {
+        res.status(200).end();
+        console.log("it was successfully deleted")
+        }
+    });
+    });
 
 // export routes for server.js to use
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -5,6 +5,17 @@ var burger = {
         orm.all("burgers", function(res) {
             cb(res);
         })
-    }
+    },
+    // the variables colsa nd vals are arrays
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        })
+    },
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+          cb(res);
+        });
+      }
 }
 module.exports = burger;
